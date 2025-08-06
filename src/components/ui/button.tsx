3ee268498b8ff49d7a47ec1308410aabd008629b
Jotis1 <cuellarjotis@gmail.com>
2025-08-06 22:23:41 +0200

    dev: Updating @yoruvese-js/tailwindcss package version

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,15 +1,52 @@
 import { type ButtonHTMLAttributes } from "react";
 import { cn } from "../../styles/cn";
 
-type ButtonProps = {
-    variant?: "primary" | "secondary" | "ghost";
-    width?: "fit" | "full";
+export type ButtonProps = {
+    variant?: "primary" | "secondary" | "tertiary" | "danger"
+    disabled?: boolean
+    onlyIcon?: boolean
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
-export function Button({ width = "fit", variant = "primary", children, ...props }: ButtonProps) {
+export function Button({
+    variant = "primary",
+    disabled,
+    onlyIcon,
+    className,
+    children,
+    ...props
+}: ButtonProps) {
     return (
         <button
-            className={cn(props.className)}
+            className={cn(
+                className,
+                "flex items-center gap-2.5 px-5 py-2.5",
+                "transition-colors rounded-xl text-sm leading-sm font-semibold cursor-pointer",
+                onlyIcon && "px-2.5 py-2.5",
+                variant === "primary" && [
+                    "shadow-sm",
+                    disabled ?
+                        "bg-disabled outline outline-disabled text-disabled cursor-not-allowed" :
+                        "bg-brand-solid hover:bg-brand-solid-hover text-primary-on-brand"
+                ],
+                variant === "secondary" && [
+                    "shadow-sm",
+                    disabled ?
+                        "bg-primary outline outline-disabled text-disabled cursor-not-allowed" :
+                        "bg-primary hover:bg-primary-hover text-secondary hover:text-secondary-hover outline outline-primary"
+                ],
+                variant === "tertiary" && [
+                    disabled ?
+                        "bg-primary text-disabled cursor-not-allowed" :
+                        "bg-primary hover:bg-primary-hover text-secondary hover:text-secondary-hover"
+                ],
+                variant === "danger" && [
+                    "shadow-sm",
+                    disabled ?
+                        "bg-primary outline outline-disabled text-disabled cursor-not-allowed" :
+                        "bg-primary hover:bg-primary-hover text-error-primary outline outline-error-subtle"
+                ]
+            )}
+            {...props}
         >
             {children}
         </button>
